fce1090d7c0a6721d12784932a717eccd80b0dba
Derek Mullins <dmullins2@netjets.com>
2021-12-09 16:02:25 -0500

    day 7 part 2

--- day-07/index.js
+++ day-07/index.js
@@ -4,11 +4,27 @@ const positions = fs.readFileSync('./input.txt', 'utf-8').toString().split(',')
   .map(numStr => Number(numStr))
   .sort((a, b) => a - b);
 
-const mid = positions[positions.length / 2];
+{
+  const mid = positions[positions.length / 2];
 
-let total = 0;
-for (let i = 0; i < positions.length; i++) {
-  total += Math.abs(mid - positions[i]);
+  let total = 0;
+  for (let i = 0; i < positions.length; i++) {
+    total += Math.abs(mid - positions[i]);
+  }
+
+  console.log('Solution 1', total);
 }
 
-console.log('Solution 1', total);
+{
+  let distances = [];
+
+  for (let i = 0; i < positions[positions.length-1]; i++) {
+    distances[i] = 0;
+    positions.forEach((position) => {
+      const delta = Math.abs(position - i);
+      distances[i] += delta / 2 * (delta + 1);
+    });
+  }
+
+  console.log('Solution 2', Math.min(...distances));
+}
